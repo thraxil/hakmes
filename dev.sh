e3b4b47353647936e2c7122b0d26c4fa654062c4
Anders Pearson <anders@columbia.edu>
2015-01-19 12:34:45 +0100

    testing different chunk sizes to optimize
    on my machine, 16MB appears to work best.
    
    That's against a cluster of 6 cask nodes, running on USB drives
    and 8CPU cores. Everything on localhost.
    
    size  clean upload   cached upload  download
    1mb   1m52s          18s            2.8s
    4mb   26s            14s            1.0s
    16mb  21s            15s            1.0s
    32mb  35s            12s            2.4s
    
    So, all the speeds improve as sizes go up, until
    we pass 16mb, then it starts to go down.
    
    Test done with a 500MB file.
    
    I encourage you to run similar tests on your
    own system as I imagine it will vary wildly depending
    on the cask cluster you are running and typical
    file sizes.

--- dev.sh
+++ dev.sh
@@ -3,6 +3,15 @@
 make
 export HAKMES_PORT=9300
 export HAKMES_CASK_BASE=http://localhost:9201
-export HAKMES_CHUNK_SIZE=1024
+# 1kb
+#export HAKMES_CHUNK_SIZE=1024
+# 1Mb
+#export HAKMES_CHUNK_SIZE=1048576
+# 4MB
+#export HAKMES_CHUNK_SIZE=4194304
+# 16MB (fastest tested on my system for large files)
+export HAKMES_CHUNK_SIZE=16777216
+# 32MB
+#export HAKMES_CHUNK_SIZE=33554432
 export HAKMES_DB_PATH=hakmes.db
 ./hakmes
